1671d31dadfd8418d906a1beb1ebd53aaccdd52a
Shreyas Kulkarni <shreyaspkulkarni7@gmail.com>
2022-10-26 21:59:12 -0400

    Changes to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,8 +106,7 @@ const Footer = () => {
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
-            Designed &amp; Built by Brittany Chiang<br></br>
-            Revised by Chandrika Deb
+            Template by Brittany Chiang
           </div>
 
           {githubInfo.stars && githubInfo.forks && (
